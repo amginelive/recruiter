fbfe2c8dc5da359b5649f47f4c973736fe9a26d7
salient <salientcool@gmail.com>
2017-05-16 21:25:37 +0800

    Created the Job Post list page

--- apps/recruit/urls.py
+++ apps/recruit/urls.py
@@ -4,5 +4,6 @@ from recruit import views
 
 urlpatterns = (
     url(r'^$', views.HomeView.as_view(), name='home'),
-    url(r'^search$', views.SearchView.as_view(), name='search'),
+    url(r'^search/$', views.SearchView.as_view(), name='search'),
+    url(r'^job/posts/$', views.job_post_list, name='job_post_list'),
 )
--- apps/recruit/views.py
+++ apps/recruit/views.py
@@ -1,35 +1,25 @@
-from django.views.generic import View
-from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render, get_object_or_404, redirect
-from django.template import RequestContext, loader
-from django.db.models import Q
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-import re
-import os
-
-from django.conf import settings
-
-from recruit.models import Company
-from profileme.models import Candidate, Agent
-
-#from recruit.forms import CandidateForm, AgentForm, CompanyForm
-from django.contrib.auth.decorators import login_required
-
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import (
+    ListView,
+    TemplateView,
+    View,
+)
+
+from braces.views import LoginRequiredMixin
+
+from .models import JobPost
+from profileme.models import Candidate
+
 User = get_user_model()
 
-class HomeView(View):
+
+class HomeView(TemplateView):
     template_name = 'recruit/landing.html'
 
-    def get(self, request, **kwargs):
-
-        return render(request, self.template_name, {
-
-        }) 
 
 class SearchView(View):
     template_name = 'recruit/search.html'
@@ -37,7 +27,7 @@ class SearchView(View):
     def get(self, request, **kwargs):
         search_string = request.GET.get('search', None)
         candidates = []
-        print(search_string)
+
         if search_string is not None:
             candidates = Candidate.objects.filter(Q(skills__iexact=search_string) |
                                               Q(title__iexact=search_string))
@@ -45,8 +35,22 @@ class SearchView(View):
         return render(request, self.template_name, {
             'candidates': candidates
         })
-        
+
     @method_decorator(login_required(login_url='/accounts/login/'))
     def dispatch(self, *args, **kwargs):
         return super(SearchView, self).dispatch(*args, **kwargs)
-        
+
+
+class JobPostListView(ListView, LoginRequiredMixin):
+    """
+    View for displaying the list of all the company's job posts.
+    """
+    model = JobPost
+    context_object_name = 'job_posts'
+    template_name = 'recruit/job_posts/list.html'
+
+    def get_queryset(self):
+        job_posts = JobPost.objects.filter(company=self.request.user.agent.company).order_by('-updated_at')
+        return job_posts
+
+job_post_list = JobPostListView.as_view()
