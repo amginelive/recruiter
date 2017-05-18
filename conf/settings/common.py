40c39f1837fb6431dce92662ee3a6580ef4d3860
salient <salientcool@gmail.com>
2017-05-18 11:01:30 +0800

    Updated the email backend configuration

--- conf/settings/common.py
+++ conf/settings/common.py
@@ -265,8 +265,7 @@ ACCOUNT_USERNAME_VALIDATORS = None
 
 NOREPLY_EMAIL = 'noreply@squareballoon.com'
 DEFAULT_SUPPORT_EMAIL = 'support@squareballoon.com'
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'tmp_mail')
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
 
 ####################################################################################################
