48bbc6c18eadd622b1091fb73c82615482ab1ecc
salient <salientcool@gmail.com>
2017-05-31 15:35:37 +0700

    Reconnecting websocket and echo consumer.

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -2,4 +2,16 @@ from channels.generic.websockets import JsonWebsocketConsumer
 
 
 class ChatServer(JsonWebsocketConsumer):
-    pass
+    http_user = True
+
+    def connection_groups(self, **kwargs):
+        return [str(self.message.user.id)]
+
+    def connect(self, message, **kwargs):
+        self.send({'accept': True})
+
+    def receive(self, content, **kwargs):
+        self.send(content)
+
+    def disconnect(self, message, **kwargs):
+        pass
