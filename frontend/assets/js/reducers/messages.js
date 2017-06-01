111493d4f5a06ea71281af94f1878b1a76e41afa
salient <salientcool@gmail.com>
2017-06-01 22:39:58 +0700

    Include current conversation id in store.

--- frontend/assets/js/reducers/messages.js
+++ frontend/assets/js/reducers/messages.js
@@ -5,12 +5,18 @@ import * as config from '../config.js';
 
 const { messageTypes } = config;
 
-const messages = (state = new Immutable.List(), action) => {
+const messages = (state = new Immutable.Map().withMutations(ctx => ctx.set('activeChat', 0).set('messageList', new Immutable.List())), action) => {
     if (action.type === messageTypes.initChat) {
-        return Immutable.fromJS(action.payload);
+        let conversation_id = 0;
+        if (action.payload.length > 0) {
+            conversation_id = action.payload[0]['conversation_id'];
+        }
+        return new Immutable.Map().withMutations(ctx => ctx.set('activeChat', conversation_id).set('messageList', Immutable.fromJS(action.payload)));
     }
-    if (action.type === messageTypes.newMessage) {
-        return state.push(Immutable.fromJS(action.payload));
+    if (action.type === messageTypes.newMessage && action.payload['conversation_id'] === state.get('activeChat')) {
+        // TODO: There may be an optimization here, consult Immutable.JS docs
+        const messageList = state.get('messageList').push(Immutable.fromJS(action.payload))
+        return state.set('messageList', messageList);
     }
     return state;
 };
