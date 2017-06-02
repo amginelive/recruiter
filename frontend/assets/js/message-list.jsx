426dcf16faa296ad7e20a394ab0e090d4d453243
salient <salientcool@gmail.com>
2017-06-02 16:19:02 +0700

    Scrollbar for message list.

--- frontend/assets/js/message-list.jsx
+++ frontend/assets/js/message-list.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import Message from './message.jsx';
 
@@ -7,15 +8,37 @@ import Message from './message.jsx';
 class MessageList extends React.Component {
     constructor(props) {
         super(props);
+
+        this.scrollList = this.scrollList.bind(this);
+    }
+
+    scrollList(value) {
+        this.scroll.scrollTop(value);
+    }
+
+    componentDidUpdate() {
+        setTimeout(
+            () => {
+                this.scrollList(this.scroll.getScrollHeight());
+            },
+            10
+        );
     }
 
     render() {
         const { messages } = this.props;
         return (
-            <div className='message-list'>
-                {messages.get('messageList').map((message, index) => {
-                    return <Message key={index} user={message.get('user')} text={message.get('text')} time={message.get('time')}/>;
-                }).toArray()}
+            <div className='message-list-container'>
+                <Scrollbars ref={(scroll) => {this.scroll = scroll;}}
+                            style={{height: 500 + 'px'}}
+                            autoHide autoHideTimeout={1000}
+                            autoHideDuration={200}>
+                    <div className='message-list'>
+                        {messages.get('messageList').map((message, index) => {
+                            return <Message key={index} user={message.get('user')} text={message.get('text')} time={message.get('time')}/>;
+                        }).toArray()}
+                    </div>
+                </Scrollbars>
             </div>
         );
     }
