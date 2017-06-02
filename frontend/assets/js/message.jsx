d3816c37992f41d0504fce6c3c1edc0334b61f93
salient <salientcool@gmail.com>
2017-06-02 16:39:36 +0700

    Format time in message.

--- frontend/assets/js/message.jsx
+++ frontend/assets/js/message.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 const Message = props => {
     return (
         <div className='message-list-item'>
             <span className='message-list-item-name'>{props.user.get('name')}</span>
             <p className='message-list-item-text'>{props.text}</p>
-            <span className='message-list-item-time'>{props.time}</span>
+            <span className='message-list-item-time'>{moment(props.time, moment.ISO_8601).format('h:mm:ss a')}</span>
         </div>
     );
 };
