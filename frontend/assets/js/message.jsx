8bf445596a79dbb696cba19972f8625ae91b6093
salient <salientcool@gmail.com>
2017-06-02 16:46:16 +0700

    Inner message layout.

--- frontend/assets/js/message.jsx
+++ frontend/assets/js/message.jsx
@@ -4,9 +4,11 @@ import moment from 'moment';
 const Message = props => {
     return (
         <div className='message-list-item'>
-            <span className='message-list-item-name'>{props.user.get('name')}</span>
+            <div className='message-list-item-header'>
+                <span className='message-list-item-name'>{props.user.get('name')}</span>
+                <span className='message-list-item-time'>{moment(props.time, moment.ISO_8601).format('h:mm a')}</span>
+            </div>
             <p className='message-list-item-text'>{props.text}</p>
-            <span className='message-list-item-time'>{moment(props.time, moment.ISO_8601).format('h:mm:ss a')}</span>
         </div>
     );
 };
