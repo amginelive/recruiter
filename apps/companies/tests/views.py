e994802e17a5d92a3f7fd2fe3e1a1b72138fd141
salient <salientcool@gmail.com>
2017-07-16 18:50:37 +0800

    Created the unit tests for the company detail page

--- apps/companies/tests/views.py
+++ apps/companies/tests/views.py
@@ -10,12 +10,25 @@ from django.core.urlresolvers import reverse
 from django_dynamic_fixture import G
 from PIL import Image
 
+from chat.models import (
+    Conversation,
+    Message,
+    Participant,
+)
 from core.tests import BaseTest
 from companies.models import (
     Company,
     CompanyInvitation,
+    CompanyRequestInvitation,
 )
-from users.models import Agent
+from recruit.models import (
+    Connection,
+    ConnectionRequest,
+)
+from users.models import (
+    Agent,
+    UserNote,
+)
 
 
 User = get_user_model()
@@ -176,6 +189,187 @@ class CompanyViewTests(BaseTest):
 
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('recruit:dashboard'))
+
+    def test_company_detail_page_company_owner(self):
+        self.client.login(username=self.user_candidate.email, password='candidate')
+
+        user = G(
+            User,
+            first_name='agent2',
+            last_name='agent2',
+            email='agent2@agent2.com',
+            password=hashers.make_password('agent2'),
+            account_type=User.ACCOUNT_AGENT
+        )
+        G(
+            Agent,
+            user=user,
+            company=None
+        )
+
+        company_request_invitation = G(
+            CompanyRequestInvitation,
+            company=self.company,
+            user=user
+        )
+
+        user_note = G(
+            UserNote,
+            note_by=self.user_candidate,
+            note_to=self.company.owner.user
+        )
+
+        connection = G(
+            Connection,
+            connectee=self.user_candidate,
+            connecter=self.company.owner.user
+        )
+
+        connection_request = G(
+            ConnectionRequest,
+            connectee=self.user_candidate,
+            connecter=self.company.owner.user
+        )
+
+        conversation = G(
+            Conversation,
+            owner=self.user_candidate,
+            conversation_type=Conversation.CONVERSATION_USER
+        )
+
+        Participant.objects.create(
+            status=Participant.PARTICIPANT_ACCEPTED,
+            user=self.company.owner.user,
+            conversation=conversation
+        )
+        Participant.objects.create(
+            status=Participant.PARTICIPANT_ACCEPTED,
+            user=self.user_candidate,
+            conversation=conversation
+        )
+
+        message_agent = G(
+            Message,
+            author=self.company.owner.user,
+            conversation=conversation
+        )
+        message_candidate = G(
+            Message,
+            author=self.user_candidate,
+            conversation=conversation
+        )
+
+        response = self.client.get(reverse('companies:company_detail', kwargs={'slug': self.company.slug}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context.get('company'), self.company)
+        self.assertIn(company_request_invitation, response.context.get('invitation_requests'))
+        self.assertEqual(response.context.get('current_agent'), self.company.owner)
+
+        self.assertEqual(response.context.get('user_note'), UserNote)
+        self.assertIn(user_note, response.context.get('user_notes'))
+
+        self.assertEqual(response.context.get('current_agent_connection'), connection)
+        self.assertEqual(response.context.get('connection_request'), ConnectionRequest)
+        self.assertEqual(response.context.get('current_agent_connection_request'), connection_request)
+
+        self.assertEqual(response.context.get('first_contact_sent'), message_candidate)
+        self.assertEqual(response.context.get('last_message_sent'), message_candidate)
+        self.assertEqual(response.context.get('last_message_received'), message_agent)
+        self.assertEqual(response.context.get('last_person_in_contact').user, self.company.owner.user)
+        self.assertEqual(response.context.get('last_person_added_manual_track'), self.company.owner.user)
+
+    def test_company_detail_page_company_staff(self):
+        self.client.login(username=self.user_candidate.email, password='candidate')
+
+        new_agent_user = G(
+            User,
+            first_name='agent2',
+            last_name='agent2',
+            email='agent2@agent2.com',
+            password=hashers.make_password('agent2'),
+            account_type=User.ACCOUNT_AGENT
+        )
+        new_agent = G(
+            Agent,
+            user=new_agent_user,
+            company=self.company
+        )
+
+        company_request_invitation = G(
+            CompanyRequestInvitation,
+            company=self.company,
+            user=new_agent_user
+        )
+
+        user_note = G(
+            UserNote,
+            note_by=self.user_candidate,
+            note_to=new_agent_user
+        )
+
+        connection = G(
+            Connection,
+            connectee=self.user_candidate,
+            connecter=new_agent_user
+        )
+
+        connection_request = G(
+            ConnectionRequest,
+            connectee=self.user_candidate,
+            connecter=new_agent_user
+        )
+
+        conversation = G(
+            Conversation,
+            owner=self.user_candidate,
+            conversation_type=Conversation.CONVERSATION_USER
+        )
+
+        Participant.objects.create(
+            status=Participant.PARTICIPANT_ACCEPTED,
+            user=new_agent_user,
+            conversation=conversation
+        )
+        Participant.objects.create(
+            status=Participant.PARTICIPANT_ACCEPTED,
+            user=self.user_candidate,
+            conversation=conversation
+        )
+
+        message_agent = G(
+            Message,
+            author=new_agent_user,
+            conversation=conversation
+        )
+        message_candidate = G(
+            Message,
+            author=self.user_candidate,
+            conversation=conversation
+        )
+
+        response = self.client.get(
+            reverse('companies:company_detail', kwargs={'slug': self.company.slug}),
+            {'agent': new_agent.pk}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context.get('company'), self.company)
+        self.assertIn(company_request_invitation, response.context.get('invitation_requests'))
+        self.assertEqual(response.context.get('current_agent'), new_agent)
+
+        self.assertEqual(response.context.get('user_note'), UserNote)
+        self.assertIn(user_note, response.context.get('user_notes'))
+
+        self.assertEqual(response.context.get('current_agent_connection'), connection)
+        self.assertEqual(response.context.get('connection_request'), ConnectionRequest)
+        self.assertEqual(response.context.get('current_agent_connection_request'), connection_request)
+
+        self.assertEqual(response.context.get('first_contact_sent'), message_candidate)
+        self.assertEqual(response.context.get('last_message_sent'), message_candidate)
+        self.assertEqual(response.context.get('last_message_received'), message_agent)
+        self.assertEqual(response.context.get('last_person_in_contact').user, new_agent_user)
+        self.assertEqual(response.context.get('last_person_added_manual_track'), new_agent_user)
 
 
 class CompanyInviteViewTests(BaseTest):
